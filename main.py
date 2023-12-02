dbe671b5f224554c17208748a9ff816dd2d0842b
LOGIKA <b56642694@gmail.com>
2023-12-02 13:11:23 +0200

    1

--- main.py
+++ main.py
@@ -8,4 +8,16 @@ window = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Maze')
 
 bg = pygame.transform.scale(pygame.image.load('img/bg.png'), (width, height))
-window.blit(bg, (0, 0))
+
+
+GameOver = False
+while not GameOver:
+    window.blit(bg, (0, 0))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            GameOver = True
+
+    pygame.display.update()
+
+pygame.quit()
