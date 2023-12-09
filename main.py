d86aab6817eb5d4811ecd7aa13807307717cf9c1
LOGIKA <danilltrotsenko2002@gmail.com>
2023-12-09 13:18:54 +0200

    2

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 import pygame
 pygame.init()
 
+
+class Sprite:
+    def __init__(self, x, y, filename):
+        self.x = x
+        self.y = y
+        self.width = 70
+        self.height = 70
+        self.hitbox = pygame.Rect(self.x, self.y, self.width, self.height)
+        self.image = pygame.image.load(filename)
+        self.image = pygame.transform.scale(self.image, (self.width, self.height))
+
+    def draw(self, wnd):
+        wnd.blit(self.image, (self.hitbox.x, self.hitbox.y))
+
+
 width = 800
 height = 600
 
@@ -9,6 +24,7 @@ pygame.display.set_caption('Maze')
 
 bg = pygame.transform.scale(pygame.image.load('img/bg.png'), (width, height))
 
+fox = Sprite(10, 10, 'img/fox.png')
 
 GameOver = False
 while not GameOver:
@@ -17,6 +33,8 @@ while not GameOver:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             GameOver = True
+
+    fox.draw(window)
 
     pygame.display.update()
 
