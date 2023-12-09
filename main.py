59df4a5c984ebc23acdfed6e4c7a33be00f8ace2
LOGIKA <danilltrotsenko2002@gmail.com>
2023-12-09 13:34:25 +0200

    2

--- main.py
+++ main.py
@@ -16,6 +16,29 @@ class Sprite:
         wnd.blit(self.image, (self.hitbox.x, self.hitbox.y))
 
 
+class Player(Sprite):
+    def __init__(self, x, y, filename):
+        super().__init__(x, y, filename)
+        self.step = 5
+
+    def move_right(self):
+        self.hitbox.x += self.step
+
+    def move_left(self):
+        self.hitbox.x -= self.step
+
+    def move_up(self):
+        self.hitbox.y -= self.step
+
+    def move_down(self):
+        self.hitbox.y += self.step
+
+
+class Enemy(Sprite):
+    def __init__(self, x, y, filename):
+        super().__init__(x, y, filename)
+
+
 width = 800
 height = 600
 
@@ -24,7 +47,8 @@ pygame.display.set_caption('Maze')
 
 bg = pygame.transform.scale(pygame.image.load('img/bg.png'), (width, height))
 
-fox = Sprite(10, 10, 'img/fox.png')
+player = Player(0, 0, 'img/fox.png')
+enemy = Enemy(730, 530, 'img/bunny.png')
 
 GameOver = False
 while not GameOver:
@@ -33,8 +57,21 @@ while not GameOver:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             GameOver = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                player.move_right()
 
-    fox.draw(window)
+            if event.key == pygame.K_LEFT:
+                player.move_left()
+
+            if event.key == pygame.K_UP:
+                player.move_up()
+
+            if event.key == pygame.K_DOWN:
+                player.move_down()
+
+    player.draw(window)
+    enemy.draw(window)
 
     pygame.display.update()
 
